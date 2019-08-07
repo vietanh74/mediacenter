4f4ae3c63de2b02ea08b1dd5cec641e2d6968843
Viet Anh <viet.anh@setacinq.vn>
2019-08-07 15:28:08 +0700

    aaaa

---
+++ src/migrations/2019_08_07_152558_create_media_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateMediaTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('media', function(Blueprint $table)
+		{
+			$table->char('id', 32)->primary();
+			$table->string('mime_type', 32)->nullable();
+			$table->string('original_name')->nullable();
+			$table->integer('size')->nullable();
+			$table->char('updated_by', 32)->nullable();
+			$table->char('created_by', 32)->nullable();
+			$table->timestamps();
+			$table->string('width', 225)->nullable();
+			$table->string('height', 225)->nullable();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('media');
+	}
+
+}
